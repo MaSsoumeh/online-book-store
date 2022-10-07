1c608f9136b68d5736b8315cc10808ca6ad23fca
massoumeh <massoumehzarei@gmail.com>
2022-10-08 02:20:48 +0330

    💅🏻 create global theme and wrap app with theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "@mui/material/styles";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import store from "../redux/store";
+import "../styles/globals.css";
+import { theme } from "../styles/theme";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Navbar />
+        <div
+          style={{
+            position: "relative",
+            top: "70px",
+            marginBottom: "200px",
+          }}
+        >
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </Provider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0D226B",
+      light: "#F2F7FF",
+      dark: "#0B409C",
+    },
+    secondary: {
+      main: "#FDBE34",
+    },
+  },
+});
