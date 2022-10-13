036b6a7bfcc945c2b09dba9823c82e2e6a027772
massoumeh <massoumehzarei@gmail.com>
2022-10-14 00:09:13 +0330

    doc: mock data for categories

---
+++ data/bookCategories.js
@@ -0,0 +1,14 @@
+export const categories = [
+  { image: "/images/art/cover.webp", name: "Art", id: "1" },
+  {
+    image: "/images/children/cover.webp",
+    name: "Children",
+    id: "2",
+  },
+  { image: "/images/pets/cover.jpeg", name: "Pets", id: "4" },
+  {
+    image: "/images/food/cover.jpeg",
+    name: "Food And Drinks",
+    id: "3",
+  },
+];
