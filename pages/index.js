0ebffcf65f5556fd6232fea45c4786281dbbd759
masoumeh zarei <masoumeh@smartech.ir>
2022-10-16 19:18:30 +0330

    feat: add search input to the pages 🔍

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,16 @@ import CategoryCard from "../components/CategoryCard";
 import { theme } from "../styles/theme";
 import { categories } from "../data/bookCategories";
 import PageHeader from "../components/PageHeader";
+import SearchInput from "../components/SearchInput";
 
 const HomePage = () => {
   return (
     <main style={styles.mainWrapper}>
       <PageHeader title="Book Categories" />
+      <SearchInput
+        dedicatedStyle={{ marginBottom: "36px" }}
+        placeholder="Search for books or authors..."
+      />
       <section style={styles.section}>
         {categories.map((category) => {
           return (
@@ -23,8 +28,12 @@ const HomePage = () => {
 };
 export default HomePage;
 
-const styles = {
+export const styles = {
   mainWrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
     color: theme.palette.primary.dark,
   },
   section: {
--- pages/shop.js
+++ pages/shop.js
@@ -1,39 +1,26 @@
 import BookCard from "../components/BookCard";
+import BookWrapper from "../components/BookWrapper";
+import PageHeader from "../components/PageHeader";
+import SearchInput from "../components/SearchInput";
 import { theme } from "../styles/theme";
 import { getBooks } from "./api/books";
-
+import { styles } from ".";
 const ShopPage = ({ books }) => {
   return (
-    <main style={{ color: theme.palette.primary.dark }}>
-      <p
-        style={{
-          textAlign: "center",
-          background: theme.palette.secondary.main,
-        }}
-      >
-        All Books
-      </p>
-      <section
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: 24,
-          paddingRight: 24,
-          paddingLeft: 24,
-          marginBottom: 100,
-          justifyContent: "flex-start",
-          marginTop: 36,
-        }}
-      >
+    <main style={{ ...styles.mainWrapper }}>
+      <PageHeader title="All Books" />
+      <SearchInput placeholder="Search for books or authors..." />
+      <BookWrapper>
         {books.map((book) => {
           return <BookCard book={book} key={book.id} />;
         })}
-      </section>
+      </BookWrapper>
     </main>
   );
 };
 
 export default ShopPage;
+
 export async function getStaticProps() {
   const books = await getBooks();
   return { props: { books } };
